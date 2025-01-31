beb5a2180662f5a453bfe417c4f2b62664639404
skyegarcia <161562153+skyegarcia@users.noreply.github.com>
2025-01-31 01:40:11 +0000

    first commit

---
+++ src/MixMatch.java
@@ -0,0 +1,20 @@
+public class MixMatch{
+    public static void main(String[] args){
+    String rand = "Java";
+    System.out.println(rand + ": String - Stores a sequence of characters.");
+    char rand2 = 'J';
+    System.out.println(rand2 + ": Char - Stores a single character.");
+    short a = 1;
+    System.out.println(a + ": Short - Stores whole numbers.");
+    int b = 2;
+    System.out.println(b + ": Int - Stores whole number, without the decimal component.");
+    long c = 3456789;
+    System.out.println(c + ": Long - Stores large whole numbers.");
+    float x = 0.1f;
+    System.out.println(x + ": Float - Stores decimal numbers.");
+    double in_java = 100.0;
+    System.out.println(in_java + ": Double - Stores decimal numbers, upto 15.");
+    boolean choice = true;
+    System.out.println(choice + ": Boolean - Stores only boolean variables such as true or false.");
+    }
+}
